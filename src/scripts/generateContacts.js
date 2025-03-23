49df56e8fd7087b4952a3e5c70da61874afac226
hanna_tarasenko <en.anya.tarasenko@gmail.com>
2025-03-24 00:41:27 +0200

    hw1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const generateContacts = async (number) => {
+  const contactList = await readContacts();
+  console.log(contactList);
+  //   const contact = createFakeContact();
+
+  // метод fill заповнює масив
+  const newContacts = Array(number)
+    .fill(0)
+    .map(() => createFakeContact());
+  await writeContacts([...contactList, ...newContacts]);
+  //   console.log(contactList);
+  // console.log(contact);
+};
 
 generateContacts(5);
